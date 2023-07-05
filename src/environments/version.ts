af6248f0b75bbf9ec94e729bd04c4636d2f432ed
larinleo <bolivar.soret@gmail.com>
2023-07-05 12:19:01 -0400

    Add: version

---
+++ src/environments/version.ts
@@ -0,0 +1,14 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export const VERSION = {
+    "dirty": false,
+    "raw": "d58341d",
+    "hash": "d58341d",
+    "distance": null,
+    "tag": null,
+    "semver": null,
+    "suffix": "d58341d",
+    "semverString": null,
+    "version": "2.0.1"
+};
+/* tslint:enable */
