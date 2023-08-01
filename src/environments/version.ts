561fa7bff8c43ca60367ac8b9bb436ce43d3837d
larinleo <bolivar.soret@gmail.com>
2023-08-01 09:26:17 -0400

    Enable: access control

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,13 +1,13 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": false,
-    "raw": "98ae6c3",
-    "hash": "98ae6c3",
+    "dirty": true,
+    "raw": "606d636-dirty",
+    "hash": "606d636",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "98ae6c3",
+    "suffix": "606d636-dirty",
     "semverString": null,
     "version": "2.0.17"
 };
